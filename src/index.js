135a109baa882a123a97d0146911afe2fe2618e0
conbec <56614504+conbec@users.noreply.github.com>
2020-05-29 16:21:09 -0700

    beginning of skeleton

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(
+  <Router><App /></Router>,
+  root
+)
