095976c7f4a5a6d02f431ba5e15d461cb59450d6
conbec <56614504+conbec@users.noreply.github.com>
2020-06-17 21:01:31 -0700

    more auth done, still not finished

---
+++ apis/auth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const authRouter = express.Router();
+const {
+  findUserFromToken,
+  authenticate,
+  compare,
+  hash
+} = require('../data/auth')
+
+authRouter.post('/', async (req, res, next) => {
+  try {
+    const token = await authenticate(req.body)
+    res.status(201).send(token);
+  } catch (error) {
+    next(error);
+  }
+})
+
+module.exports = authRouter;
+
+/*
+app.post('/api/auth', (req, res, next)=> {
+  db.authenticate(req.body)
+    .then( token => res.send({ token }))
+    .catch( ()=> {
+      const error = Error('not authorized');
+      error.status = 401;
+      next(error);
+    } );
+})
+
+*/
--- src/Nav.js
+++ src/Nav.js
@@ -1,44 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import Login from './Login';
 
 const Nav = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isUser, setIsUser] = useState(true);
 
-  const URLizeEmail = (mail) => {
-    mail = mail.replace(/\./gi, '%2E');
-    mail = mail.replace(/@/gi, '%40');
-    return mail;
-  }
-
-  const checkCredentials = async (event) => {
-    event.preventDefault();
-    const mail = URLizeEmail(email);
-    const user = await axios.get(`/user/${mail}`);
-    if (!user) {
-      setIsUser(false);
-    } else {
-      //do login stuff 
-    }
-  }
 
   return (
     <div id="nav">
-      <form id="login" onSubmit={(ev) => checkCredentials(ev)}>
-        <input type="email" placeholder="email" value={email} onChange={(ev) => setEmail(ev.target.value)} />
-        <input type="text" placeholder="password" value={password} onChange={(ev) => setPassword(ev.target.value)} />
-        <input type="submit" value="Submit" />
-      </form>
-      {!isUser &&
-      <form>
-        <input type="text" placeholder="first name" value={firstName} onChange={(ev) => setFirstName(ev.target.value)} />
-        <input type="text" placeholder="last name" value={lastName} onChange={(ev) => setLastName(ev.target.value)} />
-        <input type="text" placeholder="street" value={street} onChange={(ev) => setStreet(ev.target.value)} />
-        <input type="text" placeholder="city" value={city} onChange={(ev) => setCity(ev.target.value)} />
-        <input type="text" placeholder="state" value={state} onChange={(ev) => setState(ev.target.value)} />
-        <input type="text" placeholder="zipcode" value={zipcode} onChange={(ev) => setZipcode(ev.target.value)} />
-      </form>}
+      <Login />
     </div>
   )
 }
