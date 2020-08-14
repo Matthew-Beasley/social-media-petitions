9cd752c681df337d331c63685765515615abef8c
conbec <56614504+conbec@users.noreply.github.com>
2020-08-14 15:29:42 -0700

    started on petition tests

--- test/petitionApi.test.js
+++ test/petitionApi.test.js
@@ -1,38 +1,22 @@
+const { deleteUser } = require('../data/crud/users');
 const {
-  createUser,
-  updateUser,
-  deleteUser
-} = require('../data/crud/users');
-const { authenticate } = require('../data/auth');
-const { TestScheduler } = require('jest');
+  createPetitions,
+  readPetition,
+  readAllPetitions,
+  deletePetition
+} = require('../data/crud/petitions');
 const axios = require('axios');
 process.env.JWT = 'foobar';
-
-
-const headers = () => {
-  return {
-    headers: {
-      authorization: token
-    }
-  };
-};
-
 const url = 'http://localhost:3000';
-let user = null;
-let token = null;
 
-const authorizeUser = async () => {
-  user = await createUser({ email: 'sam@email.com', password: 'jasper' });
-  token = await authenticate({ email: 'sam@email.com', password: 'jasper' });
-}
-
-const authorizeAdmin = async () => {
-  await createUser({ email: 'sam@email.com', password: 'jasper' });
-  user = await updateUser({ email: 'sam@email.com', isAdmin: true })
-  token = await authenticate({ email: 'sam@email.com', password: 'jasper' });
-}
+const {
+  authorizeAdmin,
+  authorizeUser,
+  headers
+} = require('./testAuthorization');
 
 afterEach(async () => {
   await deleteUser({ email: 'sam@email.com' });
-})
+  await deletePetition({ title: 'Donald Trump is a despot' })
+});
 
