7da9b56594629906a705a9524ada9c5ddde92dd5
conbec <56614504+conbec@users.noreply.github.com>
2020-10-01 17:35:11 -0700

    homeview styled

--- apis/news.js
+++ apis/news.js
@@ -4,11 +4,12 @@ const redis = require('redis')
 const redisClient = redis.createClient();
 const newsRouter = express.Router();
 
-const endPoint = 'https://newsapi.org/v2/top-headlines?country=us&q=';
+const endPoint = 'https://newsapi.org/v2/top-headlines?country=us&catagory=';
 const apiKey = '&apiKey=c30ad051e9e6471490d1c763659adc0b';
 
 const checkCache = (req, res, next) => {
-  redisClient.get(req.query.arg, (err, data) => {
+  const key = `${endPoint}${req.query.arg}${apiKey}`;
+  redisClient.get(key, (err, data) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
@@ -26,10 +27,10 @@ const checkCache = (req, res, next) => {
 //https://newsapi.org/v2/top-headlines?country=us&q=trump&apiKey=c30ad051e9e6471490d1c763659adc0b
 
 newsRouter.get('/topics', checkCache, async (req, res, next) => {
-  const arg = req.query.arg;
+  const key = `${endPoint}${req.query.arg}${apiKey}`;
   try {
-    const articles = await axios.get(`${endPoint}${arg}${apiKey}`);
-    redisClient.set(arg, JSON.stringify(articles.data));
+    const articles = await axios.get(`${key}`);
+    redisClient.set(key, JSON.stringify(articles.data));
     redisClient.expire(arg, 43200);
     res.status(200).json(articles);
   } catch (error) {
--- src/News.js
+++ src/News.js
@@ -5,9 +5,10 @@ const News = () => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
     let isCancelled = false;
-    axios.get('/news/topics?arg=trump')
+    axios.get('/news/topics?arg=politics')
       .then((response) => {
         if (!isCancelled) {
+          console.log(response.data.articles)
           setArticles(response.data.articles);
         }
       });
@@ -21,7 +22,7 @@ const News = () => {
         {articles.map(article => {
             return (
               <li key={article.title}>
-                <img className="news-image" src={article.urlToImage} />
+                <img className="news-image" src={article.urlToImage ? article.urlToImage : './assets/default-article-image.jpg'} />
                 <div className="news-content">
                   <div className="news-title"><a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a></div>
                   <div className="news-description">{article.description}</div>
